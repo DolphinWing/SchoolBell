30fc3685cbb8750d3a94ce4fad3189e0d966ce8d
DolphinWing <dolphinwing74+github@gmail.com>
2026-07-15 18:02:21 +0800

    v0.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "dolphin.android.apps.schoolbell"
         minSdk = 23
         targetSdk = 37
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 4
+        versionName = "0.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
